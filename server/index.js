4519134420e3326dea15af0a1bb70352a7ffbda3
JDRengifo <josedariorf@gmail.com>
2024-04-13 15:49:40 -0300

    M34

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const server = require("./src/server");
 const { conn } = require('./src/db.js');
  const { getCreateBdCountry } = require("./src/controllers/getCreateBdCountry.js");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ;
   // conn.sync({ force: true}).then(() => {  //? uso Restringido
   conn.sync({ alter: true }).then(() => { 
 server.listen(PORT, () => {
