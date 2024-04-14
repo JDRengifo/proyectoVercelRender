0b3731a3ed064381fa9e7c2bbfda7e1d9281cef5
JDRengifo <josedariorf@gmail.com>
2024-04-13 22:58:10 -0300

    M36

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const { conn } = require('./src/db.js');
  const { getCreateBdCountry } = require("./src/controllers/getCreateBdCountry.js");
 
 const PORT = process.env.PORT || 3001;
-  // conn.sync({ force: true}).then(() => {  //? uso Restringido
-  conn.sync({ alter: true }).then(() => { 
+  conn.sync({ force: true}).then(() => {  //? uso Restringido
+  // conn.sync({ alter: true }).then(() => { 
 server.listen(PORT, () => {
-  //  getCreateBdCountry() //! FALTA ARREGLAR ESTO
+   getCreateBdCountry() //! FALTA ARREGLAR ESTO
   console.log(`Server listening on port ${PORT}`);
 })
 }).catch(error => console.error(error))
